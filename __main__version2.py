fbf085ea7bdbeb6e73ffd6299b295ed050d1047e
lingsongt <62587804+lingsongt@users.noreply.github.com>
2020-03-27 17:43:49 +1100

    Update __main__version2.py

--- __main__version2.py
+++ __main__version2.py
@@ -17,13 +17,16 @@ def main():
     print(black_list)
 
     distance_list = create_distance_list(black_list)
-    group_list = make_group(black_list, distance_list)
+    group_list = make_group(distance_list)
     print(group_list)
+
+    search_list = create_search_list(black_list, distance_list, group_list)
+    print(search_list)
 
 
 # calculate distance between two tokens
-def distance_cal(token1, token2):
-    return math.sqrt((token1[1] - token2[1]) ** 2 + (token1[2] - token2[2]) ** 2)
+def distance_cal(point1, point2):
+    return math.sqrt((point1[1] - point2[1]) ** 2 + (point1[2] - point2[2]) ** 2)
 
 
 # create a list that contains all the distance between two tokens
@@ -45,21 +48,22 @@ def create_distance_list(black_list):
 
 # putting black tokens into different groups,
 # tokens in the same group will boom together if one of the member in the group boomed
-def make_group(black_list, distance_list):
+def make_group(distance_list):
+    current_group = 0
     group_list = []
     length = len(distance_list)
 
     for i in range(0, length):
-        if not isgrouped(group_list, i):
+        if not is_grouped(group_list, i):
             group_list.append([i])
-            current_group = i
             search_surrounding(distance_list, group_list, current_group, i)
+            current_group += 1
 
     return group_list
 
 
 # check whether a token is grouped or not
-def isgrouped(group_list, token):
+def is_grouped(group_list, token):
     for group in group_list:
         if token in group:
             return True
@@ -71,10 +75,136 @@ def isgrouped(group_list, token):
 # if the distance is 1 or sqrt of 2, add this token to the group
 def search_surrounding(distance_list, group_list, current_group, token):
     for i in range(0, len(distance_list)):
-        if distance_list[token][i] == 1.0 or distance_list[token][i] ==math.sqrt(2):
+        if distance_list[token][i] == 1.0 or distance_list[token][i] == math.sqrt(2):
             if not (i in group_list[current_group]):
                 group_list[current_group].append(i)
                 search_surrounding(distance_list, group_list, current_group, i)
+
+
+# create search list by find the distance between two tokens which is 2, square root 5 or square root 8
+def create_search_list(black_list, distance_list, group_list):
+    # search_dict has a point coordinate as its keys and the item is a weight of this point
+    search_dict = {}
+    length = len(distance_list)
+
+    for i in range(0, length):
+        for j in range(i + 1, length):
+            if not is_same_group(group_list, i, j):
+                distance = distance_list[i][j]
+                # distance 2 or square root 8 has only one mid-point
+                if distance == 2.0 or distance == math.sqrt(8):
+
+                    search_point = find_search_point(black_list, 1, i, j)
+                    # more time a point has been found, more weight it has
+                    if search_point in search_dict:
+                        search_dict[search_point] += 1
+                    else:
+                        search_dict[search_point] = 0
+
+                elif distance == math.sqrt(5):
+                    # distance square root 5 has two mid-point
+                    search_point = find_search_point(black_list, 2, i, j)
+
+                    if search_point[0] in search_dict:
+                        search_dict[search_point[0]] += 1
+                    else:
+                        search_dict[search_point[0]] = 0
+
+                    if search_point[1] in search_dict:
+                        search_dict[search_point[1]] += 1
+                    else:
+                        search_dict[search_point[1]] = 0
+
+    search_list = sorted(search_dict.items(), key=lambda x: x[1], reverse=True)
+
+    return search_list
+
+
+def is_same_group(group_list, token1, token2):
+    for i in range(0, len(group_list)):
+        if (token1 in group_list[i]) and (token2 in group_list[i]):
+            return True
+
+    return False
+
+
+# the search point should be the mid point of two given point
+def find_search_point(black_list, num_point, token1, token2):
+    point1 = [black_list[token1][1], black_list[token1][2]]
+    point2 = [black_list[token2][1], black_list[token2][2]]
+    search_point = []
+
+    if num_point == 1:
+        # one search point
+        relative_pos = find_relative_pos(point1, point2)
+        search_point = (point1[0] + relative_pos[0], point1[1] + relative_pos[1])
+    else:
+        # two search point
+        if abs(point1[0] - point2[0]) == 2:
+            # horizontal distance is 2
+            # move point1 downward for 1 unit,and find one search point
+            point1[1] -= 1
+            relative_pos = find_relative_pos(point1, point2)
+            search_point1 = (point1[0] + relative_pos[0], point1[1] + relative_pos[1])
+            search_point.append(search_point1)
+            # move point1 upward for 2 unit
+            # which is 1 unit upward from original position
+            point1[1] += 2
+            relative_pos = find_relative_pos(point1, point2)
+            search_point2 = (point1[0] + relative_pos[0], point1[1] + relative_pos[1])
+            search_point.append(search_point2)
+        else:
+            # vertical distance is 2
+            # move point1 leftward for 1 unit,and find one search point
+            point1[0] -= 1
+            relative_pos = find_relative_pos(point1, point2)
+            search_point1 = (point1[0] + relative_pos[0], point1[1] + relative_pos[1])
+            search_point.append(search_point1)
+            # move point1 rightward for 2 unit
+            # which is 1 unit rightward from original position
+            point1[0] += 2
+            relative_pos = find_relative_pos(point1, point2)
+            search_point2 = (point1[0] + relative_pos[0], point1[1] + relative_pos[1])
+            search_point.append(search_point2)
+
+    return search_point
+
+
+# return the relative position of point 2 to point 1
+def find_relative_pos(point1, point2):
+    if point1[0] > point2[0]:
+        # left
+        if point1[1] > point2[1]:
+            # down
+            return -1, -1
+        elif point1[1] < point2[1]:
+            # up
+            return -1, 1
+        else:
+            # same vertical position
+            return -1, 0
+    elif point1[0] < point2[0]:
+        # right
+        if point1[1] > point2[1]:
+            # down
+            return 1, -1
+        elif point1[1] < point2[1]:
+            # up
+            return 1, 1
+        else:
+            # same vertical position
+            return 1, 0
+    else:
+        # same horizontal position
+        if point1[1] > point2[1]:
+            # down
+            return 0, -1
+        elif point1[1] < point2[1]:
+            # up
+            return 0, 1
+        else:
+            # same vertical position
+            return 0, 0
 
 
 if __name__ == '__main__':
